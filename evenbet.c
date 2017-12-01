ad8e06a800a169536a69110d5ace19561cc603ab
GOKILA DHARANI SHYAMALA <31985290+GOKILADHARANISHYAMALA@users.noreply.github.com>
2017-12-01 13:35:31 +0530

    Create evenbet.c

---
+++ evenbet.c
@@ -0,0 +1,18 @@
+# cs1
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int firstnum,lastnum,i;
+ printf("\nEnter the first number:");
+ scanf("%d",&firstnum);
+ printf("\nEnter the last number:");
+ scanf("%d",&lastnum);
+ for(i=firstnum;i<lastnum;++i)
+ {
+  if(i%2==0)
+  {
+   printf("\n%d",i);
+  }
+ }
+}
